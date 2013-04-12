c735dde19763b0984c25e4db534470b1fc3aa8cb
MailChimp Devs <dev@mailchimp.com>
2013-04-12 19:22:46 +0000

    Add description field to webhooks

--- src/Mandrill/Webhooks.php
+++ src/Mandrill/Webhooks.php
@@ -11,6 +11,7 @@ class Mandrill_Webhooks {
      *     - return[] struct the inidividual webhook info
      *         - id integer a unique integer indentifier for the webhook
      *         - url string The URL that the event data will be posted to
+     *         - description string a description of the webhook
      *         - events array The message events that will be posted to the hook
      *             - events[] string the individual message event (send, hard_bounce, soft_bounce, open, click, spam, unsub, or reject)
      *         - created_at string the date and time that the webhook was created as a UTC string in YYYY-MM-DD HH:MM:SS format
@@ -27,11 +28,13 @@ class Mandrill_Webhooks {
     /**
      * Add a new webhook
      * @param string $url the URL to POST batches of events
+     * @param string $description an optional description of the webhook
      * @param array $events an optional list of events that will be posted to the webhook
      *     - events[] string the individual event to listen for
      * @return struct the information saved about the new webhook
      *     - id integer a unique integer indentifier for the webhook
      *     - url string The URL that the event data will be posted to
+     *     - description string a description of the webhook
      *     - events array The message events that will be posted to the hook
      *         - events[] string the individual message event (send, hard_bounce, soft_bounce, open, click, spam, unsub, or reject)
      *     - created_at string the date and time that the webhook was created as a UTC string in YYYY-MM-DD HH:MM:SS format
@@ -40,8 +43,8 @@ class Mandrill_Webhooks {
      *     - events_sent integer the total number of events that have ever been sent to this webhook
      *     - last_error string if we've ever gotten an error trying to post to this webhook, the last error that we've seen
      */
-    public function add($url, $events=array()) {
-        $_params = array("url" => $url, "events" => $events);
+    public function add($url, $description=null, $events=array()) {
+        $_params = array("url" => $url, "description" => $description, "events" => $events);
         return $this->master->call('webhooks/add', $_params);
     }
 
@@ -51,6 +54,7 @@ class Mandrill_Webhooks {
      * @return struct the information about the webhook
      *     - id integer a unique integer indentifier for the webhook
      *     - url string The URL that the event data will be posted to
+     *     - description string a description of the webhook
      *     - events array The message events that will be posted to the hook
      *         - events[] string the individual message event (send, hard_bounce, soft_bounce, open, click, spam, unsub, or reject)
      *     - created_at string the date and time that the webhook was created as a UTC string in YYYY-MM-DD HH:MM:SS format
@@ -68,11 +72,13 @@ class Mandrill_Webhooks {
      * Update an existing webhook
      * @param integer $id the unique identifier of a webhook belonging to this account
      * @param string $url the URL to POST batches of events
+     * @param string $description an optional description of the webhook
      * @param array $events an optional list of events that will be posted to the webhook
      *     - events[] string the individual event to listen for
      * @return struct the information for the updated webhook
      *     - id integer a unique integer indentifier for the webhook
      *     - url string The URL that the event data will be posted to
+     *     - description string a description of the webhook
      *     - events array The message events that will be posted to the hook
      *         - events[] string the individual message event (send, hard_bounce, soft_bounce, open, click, spam, unsub, or reject)
      *     - created_at string the date and time that the webhook was created as a UTC string in YYYY-MM-DD HH:MM:SS format
@@ -81,8 +87,8 @@ class Mandrill_Webhooks {
      *     - events_sent integer the total number of events that have ever been sent to this webhook
      *     - last_error string if we've ever gotten an error trying to post to this webhook, the last error that we've seen
      */
-    public function update($id, $url, $events=array()) {
-        $_params = array("id" => $id, "url" => $url, "events" => $events);
+    public function update($id, $url, $description=null, $events=array()) {
+        $_params = array("id" => $id, "url" => $url, "description" => $description, "events" => $events);
         return $this->master->call('webhooks/update', $_params);
     }
 
@@ -92,6 +98,7 @@ class Mandrill_Webhooks {
      * @return struct the information for the deleted webhook
      *     - id integer a unique integer indentifier for the webhook
      *     - url string The URL that the event data will be posted to
+     *     - description string a description of the webhook
      *     - events array The message events that will be posted to the hook
      *         - events[] string the individual message event (send, hard_bounce, soft_bounce, open, click, spam, unsub, or reject)
      *     - created_at string the date and time that the webhook was created as a UTC string in YYYY-MM-DD HH:MM:SS format
