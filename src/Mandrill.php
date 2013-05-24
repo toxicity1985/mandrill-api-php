05f49f8ff404063f29a283191e0326532636acc8
Dustin Shields-Cloues <dustin@clou.es>
2013-05-24 17:32:54 +0000

    Add detailed error message when search services are under maintenance

--- src/Mandrill.php
+++ src/Mandrill.php
@@ -25,6 +25,7 @@ class Mandrill {
         "ValidationError" => "Mandrill_ValidationError",
         "Invalid_Key" => "Mandrill_Invalid_Key",
         "Unknown_Template" => "Mandrill_Unknown_Template",
+        "ServiceUnavailable" => "Mandrill_ServiceUnavailable",
         "Invalid_Tag_Name" => "Mandrill_Invalid_Tag_Name",
         "Invalid_Reject" => "Mandrill_Invalid_Reject",
         "Unknown_Sender" => "Mandrill_Unknown_Sender",
@@ -42,7 +43,7 @@ class Mandrill {
         $this->apikey = $apikey;
 
         $this->ch = curl_init();
-        curl_setopt($this->ch, CURLOPT_USERAGENT, 'Mandrill-PHP/1.0.29');
+        curl_setopt($this->ch, CURLOPT_USERAGENT, 'Mandrill-PHP/1.0.30');
         curl_setopt($this->ch, CURLOPT_POST, true);
         curl_setopt($this->ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($this->ch, CURLOPT_HEADER, false);
--- src/Mandrill/Exceptions.php
+++ src/Mandrill/Exceptions.php
@@ -19,6 +19,11 @@ class Mandrill_Invalid_Key extends Mandrill_Error {}
 class Mandrill_Unknown_Template extends Mandrill_Error {}
 
 /**
+ * 
+ */
+class Mandrill_ServiceUnavailable extends Mandrill_Error {}
+
+/**
  * The requested tag does not exist or contains invalid characters
  */
 class Mandrill_Invalid_Tag_Name extends Mandrill_Error {}
